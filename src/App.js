7de00f7e1795e88f4ef1704bd667a6c75f4dbc72
João Pedro Luz <joao.pedro.luz@hotmail.com>
2022-10-30 12:39:39 -0300

    refactor: app function component to class component

--- src/App.js
+++ src/App.js
@@ -4,13 +4,15 @@ import Layout from './Components/Layout';
 import ThemeProvider from './Contexts/ThemeProvider';
 import GlobalStyle from './Styles/global';
 
-function App() {
-  return (
-    <ThemeProvider>
-      <GlobalStyle />
-      <Layout />
-    </ThemeProvider>
-  );
+class App extends React.Component {
+  render() {
+    return (
+      <ThemeProvider>
+        <GlobalStyle />
+        <Layout />
+      </ThemeProvider>
+    );
+  }
 }
 
 export default App;
